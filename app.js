5306c169eee75dd709cdc65a590ae936eedac8c1
Chris Mytton <chrism@simpleweb.co.uk>
2011-03-28 15:45:46 +0100

    Allow for proxy servers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = module.exports = express.createServer(function(req, res) {
-  res.send({ip: req.connection.remoteAddress});
+  res.send({ip: req.header('x-forwarded-for') || req.connection.remoteAddress});
 });
 
 if (!module.parent) {
