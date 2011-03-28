573c30682b641b5bea8fb701552ecd5361247438
Chris Mytton <self@hecticjeff.net>
2011-03-28 01:51:32 +0100

    Listen when no parent is present

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var express = require('express');
 var app = module.exports = express.createServer(function(req, res) {
   res.send({ip: req.connection.remoteAddress});
 });
+
+if (!module.parent) {
+  app.listen(3000);
+}
