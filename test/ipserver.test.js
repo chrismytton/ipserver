659dd6bd33096e721ce4ce836096c2e7275640fc
Chris Mytton <self@hecticjeff.net>
2011-05-25 20:33:49 +0100

    remove json references

--- test/ipserver.test.js
+++ test/ipserver.test.js
@@ -1,30 +1,30 @@
 var assert = require('assert'),
-    jsonip = require('../app');
+    ipserver = require('../app');
 
 module.exports = {
   'test GET /': function() {
-    assert.response(jsonip, {
+    assert.response(ipserver, {
       url: '/'
     }, {
       body: '{"ip":"127.0.0.1"}'
     });
   },
   'test jsonp callback': function() {
-    assert.response(jsonip, {
+    assert.response(ipserver, {
       url: '/?callback=test'
     }, {
       body: 'test({"ip":"127.0.0.1"});'
     });
   },
   'test optional domain support': function() {
-    assert.response(jsonip, {
+    assert.response(ipserver, {
       url: '/?domains=1'
     }, {
       body: '{"ip":"127.0.0.1","domains":["localhost"]}'
     });
   },
   'test jsonp with domains': function() {
-    assert.response(jsonip, {
+    assert.response(ipserver, {
       url: '/?callback=domaintest&domains=1'
     }, {
       body: 'domaintest({"ip":"127.0.0.1","domains":["localhost"]});'
