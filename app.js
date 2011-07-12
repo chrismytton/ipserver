dc87a32b91a731f9f77c75f931445a7db48716a0
Chris Mytton <self@hecticjeff.net>
2011-07-12 21:53:03 +0100

    use logger

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function ipInfo(req, res, next) {
 }
 
 // Create a simple middleware stack
-var app = module.exports = express.createServer(ipInfo, function(req, res) {
+var app = module.exports = express.createServer(ipInfo, express.logger(), function(req, res) {
   res.header('Link', '<https://github.com/hecticjeff/ipserver>; rel="help"; title="Source Code on GitHub"');
   if (!req.accepts('json')) {
     res.send(req.ipInfo.ip, {'Content-Type': 'text/plain'});
