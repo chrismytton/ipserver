956d82f27ae2cdad263a698881a8216804e38c70
Chris Mytton <self@hecticjeff.net>
2011-07-12 21:33:08 +0100

    remove cluster

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-var cluster = require('cluster');
-
-cluster('app.js')
-  .in('development')
-    .set('workers', 1)
-    .use(cluster.logger('logs', 'debug'))
-    .use(cluster.debug())
-    .use(cluster.repl(8888))
-    .use(cluster.reload())
-  .in('production')
-    .set('workers', 4)
-    .use(cluster.logger())
-    .use(cluster.pidfiles())
-  .in('all')
-    .listen(3000);
+require('./app').listen(process.env.PORT);
